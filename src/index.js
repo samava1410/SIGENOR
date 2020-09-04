5371bae6e964b803e54b7677cd416b5a400457b7
Santiago Marino Vasquez <samava1410@hotmail.com>
2020-09-04 00:04:28 -0500

    Implementaciones crud de docente

--- src/index.js
+++ src/index.js
@@ -2,18 +2,67 @@ const mongoose = require('mongoose');
 const schemaDocente = require('./models/docente');
 
 //Conexion con la BD Mongo
-mongoose.connect('mongodb://localhost/SIGENOR', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/SIGENOR', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(db => console.log('Conectado a la BD SIGENOR'))
     .catch(err => console.log('Problemas de conexion de bases de datos' + err));
 
 mongoose.set('useCreateIndex', true);
 
-const docenteObj = {
-    codigoDocente: "2014",
-    nombres: "Juan M",
-    apellidos: "Medina A",
-    correo: "juan@hotmail.com"
-};
+function agregar() {
+    console.log("Vamos agregar dos docentes");
 
-const docente = new schemaDocente(docenteObj);
-docente.save();
+    docenteObj = {
+        codigoDocente: "2014",
+        nombres: "Juan M",
+        apellidos: "Medina A",
+        correo: "juan@hotmail.com"
+    };
+
+    docente = new schemaDocente(docenteObj);
+    docente.save();
+
+    docenteObj = {
+        codigoDocente: "2015",
+        nombres: "Maria M",
+        apellidos: "Acevedo A",
+        correo: "maria@hotmail.com"
+    };
+
+    docente = new schemaDocente(docenteObj);
+    docente.save();
+}
+
+function eliminarYMostrar() {
+
+    schemaDocente.find({}, function (err, docs) {
+        console.log("Se agregaron " + docs.length + " docentes");
+        console.log(docs[0].nombres);
+        console.log(docs[1].nombres);
+    });
+
+    console.log("Ahora Vamos a eliminar a Juan M");
+
+    schemaDocente.deleteOne({ codigoDocente: "2014" }, function (err) {
+        console.log("Se elimino Juan M");
+    });
+
+    schemaDocente.find({}, function (err, docs) {
+        console.log("Ahora solo quedan " + docs.length + " docentes");
+        console.log(docs[0].nombres);
+    });
+
+    schemaDocente.update(
+        {codigoDocente: "2015"},
+        {apellidos: "Morales J"}
+      ).then((rawResponse) => {
+          console.log("Se ha actualizado el docente con codigo 2015")      
+      })
+      .catch((err) => {
+        // manejar error
+      });
+
+}
+
+agregar();
+
+setTimeout(eliminarYMostrar, 3000);
